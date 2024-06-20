85c4ad69c6d3c8945fa2530af10caa4f3d79a29f
Aman Dogra <amandogra@Meenakshis-MacBook-Pro.local>
2024-06-19 19:03:12 -0700

    Added more sections on landing

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const options = {
   root: null,
   rootMargin: "0px",
-  threshold: [0.25, 1, 0],
+  threshold: [0.25, , 0.5, 0.75, 1, 0],
 };
 
 const  observer = new IntersectionObserver(handleIntersect, options);
@@ -13,7 +13,7 @@ function handleIntersect(entries) {
       document.querySelectorAll(".opacity-on-scroll").forEach((item) => {
         item.style.opacity = 1;
       });
-    } else if (entry.intersectionRatio === 1) {
+    } else if (entry.intersectionRatio === 0.5) {
       document.querySelectorAll(".opacity-on-scroll").forEach((item) => {
         item.style.opacity = 0;
       });
