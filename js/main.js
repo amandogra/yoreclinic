84e78e639c5b7bcdabe7912c02c5222eaf0a8922
Aman Dogra <a_dogra@apple.com>
2024-06-19 23:45:53 -0700

    Added the hero section

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
-const options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: [0.25, , 0.5, 0.75, 1, 0],
-};
+// TODO: Add functionality to scale and fade the logo on scroll 
+// const content = document.querySelector(".logo");
+// document.addEventListener("scroll", () => {
+//   // Calculate the scale value based on the scroll position
+//   const scrollTop = window.scrollY;
+//   console.log(scrollTop);
+//   const viewportHeight = window.visualViewport.height;
+//   let scaleValue = Math.ceil(viewportHeight/scrollTop);
+//   console.log('>', scaleValue);
+//   // Apply the scaling to the content
+//   content.style.transform = `scale(${scaleValue})`;
+//   content.style.opacity = `scale(${scaleValue}transform)`;
+// });
 
-const  observer = new IntersectionObserver(handleIntersect, options);
-
-function handleIntersect(entries) {
-  entries.forEach((entry) => {
-    console.log(entry.intersectionRatio);
-    if (entry.intersectionRatio === 0) {
-      document.querySelectorAll(".opacity-on-scroll").forEach((item) => {
-        item.style.opacity = 1;
-      });
-    } else if (entry.intersectionRatio === 0.5) {
-      document.querySelectorAll(".opacity-on-scroll").forEach((item) => {
-        item.style.opacity = 0;
-      });
-    }
-  });
-}
-
-window.addEventListener("load", createObserver, false);
+// An Intersection Observer to show hide the menu bar
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//       console.log(entry.target);
+//     if(entry.isIntersecting) {
+//       entry.target.classList.add('visible');
+//     } else {
+//       entry.target.classList.remove('visible');
+//     }
+//   });
+// });
+//
+// const targetList = document.querySelectorAll('.hidden')
+//
+// targetList.forEach((target) => observer.observe(target));
